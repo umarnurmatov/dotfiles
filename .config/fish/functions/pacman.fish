130df1911eb4d0a639bcd4c5ef40176e9fc84a82
umarnurmatov <umarkhonnur12@gmail.com>
2026-09-21 19:05:29 +0300

    [ADDED] pacman & yay aliases for color mode

---
+++ .config/fish/functions/pacman.fish
@@ -0,0 +1,3 @@
+function pacman --description 'alias pacman=pacman --color always'
+    command pacman --color always $argv
+end
---
+++ .config/fish/functions/yay.fish
@@ -0,0 +1,3 @@
+function yay --description 'alias yay=yay --color always'
+    command yay --color always $argv
+end
